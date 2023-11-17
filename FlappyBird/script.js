483c251b8d0ce5a3fbc7e74a8795f70ed0a88ea1
Anatolii <anatolii.boboc@mail.ru>
2023-11-17 08:53:24 +0100

    flappy bird is finish

--- FlappyBird/script.js
+++ FlappyBird/script.js
@@ -1,91 +1,106 @@
-const playBoard = document.querySelector('.play-board');
-const htmlMarkup = document.querySelector('.food');
-const htmlScore = document.querySelector('.score');
-const controls = document.querySelectorAll('.controls i');
+document.addEventListener("DOMContentLoaded", function () {
+    const bird = document.getElementById("bird");
+    const gameContainer = document.getElementById("game-container");
+    let pipes = [];
+    let gravity = 1;
+    let velocity = 0;
+    let lift = -15;
+    let isGameOver = false;
+    let score = 0;
 
-
-let foodX;
-let foodY;
-let snakeX = 5;
-let snakeY = 10;
-let velocityX = 0;
-let velocityY = 0;
-let snakeBody = [];
-let gameOver = false;
-let setIntervalId;
-let score = 0
-
-
-const changeFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) + 1
-    foodY = Math.floor(Math.random() * 30) + 1
-};
-
-const handleGame = () => {
-    clearInterval(setIntervalId)
-    alert('Game Over')
-    location.reload()
-};  
-
-const changeDirection = (e) => {
-    if (e.key === 'ArrowUp' && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    } else if (e.key === 'ArrowDown' && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
-    } else if (e.key === 'ArrowLeft' && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
-    } else if (e.key === 'ArrowRight' && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
-    } 
-    initGame()
-};
-
-controls.forEach(key => {
-    key.addEventListener('click', () => changeDirection({ key: key.dataset.key }))
-});
-
-const initGame = () => {
-    if (gameOver) return handleGame();
-    let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
-
-    if (snakeX === foodX && snakeY === foodY) {
-        changeFoodPosition()
-        snakeBody.push(foodX, foodY)
-        score++;
-
-        htmlScore.innerHTML = `Score ${score}`;
-    }
-   
-
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1]
-    }
-
-    snakeBody[0] = [snakeX, snakeY]
-
-    snakeX += velocityX;
-    snakeY += velocityY;
-
-    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        gameOver = true;
-    }
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        htmlMarkup += `<div class="snake" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
-            gameOver = true
+    function jump() {
+        if (!isGameOver) {
+            velocity = lift; 
         }
     }
 
-    playBoard.innerHTML = htmlMarkup
-};
+    document.addEventListener("keydown", function (event) {
+        if (event.code === "Space") {
+            jump();
+        }
+    });
 
-changeFoodPosition()
-setIntervalId = setInterval(initGame, 125);
+    function generatePipe() {
+        const pipe = document.createElement("div");
+        pipe.className = "pipe";
+        gameContainer.appendChild(pipe);
+        pipes.push(pipe);
+        pipe.style.left = gameContainer.offsetWidth + "px";
+        pipe.style.height = Math.floor(Math.random() * 300) + 50 + "px";
+    }
 
-document.addEventListener('keydown', changeDirection)
+    function movePipes() {
+        for (let i = 0; i < pipes.length; i++) {
+            pipes[i].style.left = pipes[i].offsetLeft - 3 + "px";
 
+            if (pipes[i].offsetLeft + pipes[i].offsetWidth < 0) {
+                pipes[i].remove();
+                pipes.splice(i, 1);
+                i--;
+                score++;
+            }
+        }
+    }
+
+    function checkCollision() {
+        const birdTop = bird.offsetTop;
+        const birdBottom = birdTop + bird.offsetHeight;
+    
+        if (birdTop < 0) {
+            gameOver();
+        }
+    
+        if (birdBottom > gameContainer.offsetHeight) {
+            gameOver();
+        }
+    
+        for (let i = 0; i < pipes.length; i++) {
+            const birdLeft = bird.offsetLeft;
+            const birdRight = birdLeft + bird.offsetWidth;
+    
+            const pipeTop = pipes[i].offsetTop;
+            const pipeBottom = pipeTop + pipes[i].offsetHeight;
+            const pipeLeft = pipes[i].offsetLeft;
+            const pipeRight = pipeLeft + pipes[i].offsetWidth;
+    
+            if (birdBottom > pipeTop && birdTop < pipeBottom && birdRight > pipeLeft && birdLeft < pipeRight) {
+                gameOver();
+            }
+        }
+    }
+    
+    function gameOver() {
+        isGameOver = true;
+        alert("Game Over! Score: " + score);
+        location.reload();
+    }
+
+    function updateScore() {
+        document.getElementById("score").innerText = "Score: " + score;
+    }
+
+    function gameLoop() {
+        velocity += gravity;
+        bird.style.top = bird.offsetTop + velocity + "px";
+
+        movePipes();
+        checkCollision();
+        updateScore();
+
+        if (!isGameOver) {
+            requestAnimationFrame(gameLoop);
+        }
+    }
+
+    function initializeGame() {
+        startScreen.style.display = "none";
+        gameLoop(); 
+    }
+
+    const startScreen = document.getElementById("start-screen");
+    const startButton = document.getElementById("start-button");
+
+    startButton.addEventListener("click", initializeGame);
+
+    setInterval(generatePipe, 2000);
+});
